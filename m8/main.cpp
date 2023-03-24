0c196ab78a245d19e6ea18aa5572a23d0a732d36
BuzanovNikita <nikita03buzz@gmail.com>
2023-03-24 14:03:34 +0200

    fix m8

--- m8/main.cpp
+++ m8/main.cpp
@@ -2,13 +2,13 @@
 #include <cstring>
 
 class event{
-protected:
+private:
     std::string subject_;
     int year_, month_, day_;
 public:
     event(std::string s, int year, int month, int day): subject_(s), year_(year), month_(month), day_(day){};
     virtual void change_date(int new_tear, int new_month, int new_day);
-    virtual void print_res(){std::cout << subject_ << ' ' << year_ << ' ' << month_ << ' ' << day_ << std::endl;}
+    virtual void print_res(){std::cout << subject_ << ' ' << year_ << ' ' << month_ << ' ' << day_;}
     virtual void change_grade(bool new_mark) = 0;
     virtual void change_grade(int new_mark) = 0;
     virtual ~event(){};
@@ -26,7 +26,7 @@ public:
     void change_grade(int new_mark){};
     test(std::string s, int year, int month, int day, bool mark): event(s, year, month, day), mark_(mark){};
     void print_res(){
-        std::cout << subject_ << ' ' << year_ << ' ' <<  month_ << ' ' << day_;
+        this->event::print_res();
         if(mark_) std::cout << " true\n";
         else std::cout << " false\n";
     }
@@ -38,39 +38,40 @@ class exam: public event{
 public:
     void change_grade(bool new_mark){};
     exam(std::string s, int year, int month, int day, int mark): event(s, year, month, day), mark_(mark){};
-    void print_res(){std::cout << subject_ << ' ' << year_ << ' ' <<  month_ << ' ' << day_ << " Grade " << mark_ << std::endl;}
+    void print_res(){
+        this->event::print_res();
+        std::cout << " Grade " << mark_ << std::endl;
+        }
     void change_grade(int new_mark){mark_ = new_mark;}
 };
 
 
-int main(){
+/*int main(){
     enum {NUM = 7};
-    event *session[NUM];
+        event *session[NUM];
 
-    session[0]=new test("Mathematical analysis",2022,12,23,true);
-    session[1]=new exam("Operation System",2023,1,15,2);
-    session[2]=new test("Practicum",2022,12,25,false);
-    session[3]=new exam("Differential equation",2023,1,20,3);
-    session[4]=new test("Theory of probability",2022,12,29,true);
-    session[5]=new exam("Philosophy",2022,1,24,5);
-    session[6]=new exam("Numerical methods",2023,1,11,4);
+        session[0]=new test("Mathematical analysis",2022,12,23,true);
+        session[1]=new exam("Operation System",2023,1,15,2);
+        session[2]=new test("Practicum",2022,12,25,false);
+        session[3]=new exam("Differential equation",2023,1,20,3);
+        session[4]=new test("Theory of probability",2022,12,29,true);
+        session[5]=new exam("Philosophy",2022,1,24,5);
+        session[6]=new exam("Numerical methods",2023,1,11,4);
 
-    for (int i=0;i<NUM;i++)
-        session[i]->print_res();
+        for (int i=0;i<NUM;i++)
+                session[i]->print_res();
 
-    session[2]->change_date(2023,2,25);
-    session[2]->change_grade(true);
+        session[2]->change_date(2023,2,25);
+        session[2]->change_grade(true);
 
-    session[1]->change_date(2023,3,3);
-    session[1]->change_grade(3);
+        session[1]->change_date(2023,3,3);
+        session[1]->change_grade(3);
 
-    std::cout<<std::endl;
-    for (int i=0;i<NUM;i++)
-        session[i]->print_res();
+        std::cout<<std::endl;
+        for (int i=0;i<NUM;i++)
+                session[i]->print_res();
 
-    session[0]->event::print_res();
-    for (int i=0;i<NUM;i++)
+        for (int i=0;i<NUM;i++)
         delete session[i];
-
-    return 0;
-}
+        return 0;
+}*/
